c0df6261c93bde0904928c8afabd98bc963df01e
yeokyunghyun <durudgus0806@naver.com>
2025-11-16 21:50:05 +0900

    init project

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState, useRef } from "react";
+import Stomp from "stompjs";
+import SockJS from "sockjs-client";
+
+export default function AgentPage() {
+  const [messages, setMessages] = useState<string[]>([]);
+  const clientRef = useRef<any>(null);
+
+  useEffect(() => {
+    const socket = new SockJS("http://localhost:8443/ws");
+    const client = Stomp.over(socket);
+    clientRef.current = client;
+
+    client.connect({}, () => {
+      console.log("WS connected");
+
+      client.subscribe("/topic/agent", (msg) => {
+        let body = msg.body;
+        try {
+          body = JSON.parse(msg.body);
+        } catch {}
+        setMessages((prev) => [...prev, `${body.userId}: ${body.content}`]);
+      });
+    });
+
+    return () => {
+      if (clientRef.current?.connected) {
+        clientRef.current.disconnect(() => console.log("WS disconnected"));
+      }
+    };
+  }, []);
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>상담사 화면</h1>
+      {messages.map((m, idx) => (
+        <div key={idx}>{m}</div>
+      ))}
+    </div>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  define: {
+    global: "globalThis",
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    port: 3003,
+    open: true,
+    proxy: {
+      "/api": "http://localhost:8443"
+    }
+  }
+})
